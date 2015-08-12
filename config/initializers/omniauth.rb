5dab7755252fa1659b19dd70a00d2e0619590e8a
ibrahimali123 <ibrahimali966@yahoo.com>
2015-08-13 00:37:49 +0200

    e

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1638457706426157'], ENV['608de22bb1900e8ba6f52142253f0533']
+  provider :facebook, '1638457706426157', '608de22bb1900e8ba6f52142253f0533'
 end
 
