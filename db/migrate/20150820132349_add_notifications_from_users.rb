e4ec808c2623cdc0c31977c7e24338fe8b02392d
Health-Care <ibrahim@faselty.me>
2015-08-21 14:47:32 +0200

    uploaded ok

--- db/migrate/20150820132349_add_notifications_from_users.rb
+++ db/migrate/20150820132349_add_notifications_from_users.rb
@@ -1,6 +1,5 @@
 class AddNotificationsFromUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :notifications, :integer, :default => 0
+  def change 
   	add_column :users, :can_donate, :boolean, :default => true
   end
 end
